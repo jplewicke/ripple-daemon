801bba7f31d11ac449841c389cbf48b1a3bd52d1
John Paul Lewicke <jplewicke@john-paul-lewickes-macbook.local>
2010-02-22 07:41:03 -0500

    First commit.

---
+++ examples/make_rand_bids.sh
@@ -0,0 +1,10 @@
+LIMIT=$RANDOM
+PERCENT=0.1
+head -10 nodes | awk '{print "node 1.0 " $1}' > tmp
+awk '{print "person " $1}' people >> tmp
+for ((a=1; a <= LIMIT ; a++))
+do
+  echo "$a $RANDOM $RANDOM $RANDOM $RANDOM bid" >> tmp
+done
+
+awk '$1 ~ /node/ {v[NR] = $2; n[NR] = $3 ; last = NR} $1 ~ /person/ { p[NR - last] = $2; lastp = NR - last} $6 ~ /bid/ && ($2 % last != $3 % last) {printf("bid_%d|%s,%s,%s,%0.f,%f,\n", $1,p[$1 % lastp + 1], n[$2 % last + 1], n[$3 % last + 1], $4 % 100 + 1, (v[$3 % last + 1] / v[$2 % last + 1]) / (0.99 + rand() * '$PERCENT')) }' tmp
